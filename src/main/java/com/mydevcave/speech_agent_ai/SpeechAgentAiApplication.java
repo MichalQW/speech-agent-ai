d90b53f6b72abcd8654bbe10b7c7bf03522d0241
Michał Walczak <michalwalczakpoczta@gmail.com>
2025-02-23 13:21:19 +0100

    add project setup

---
+++ src/main/java/com/mydevcave/speech_agent_ai/SpeechAgentAiApplication.java
@@ -0,0 +1,13 @@
+package com.mydevcave.speech_agent_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeechAgentAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeechAgentAiApplication.class, args);
+	}
+
+}
